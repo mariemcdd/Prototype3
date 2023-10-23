d258838b62019a061e3baa8c32504d1c4b57e9e3
mariemcdd <146126998+mariemcdd@users.noreply.github.com>
2023-10-23 12:37:57 -0700

    Finish 3.4.6

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,26 +8,34 @@ public class PlayerController : MonoBehaviour
     public float GravityModifier = 1f;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public ParticleSystem ExplosionParticle;
+    public ParticleSystem DirtParticle;
+    public AudioClip JumpSound;
+    public AudioClip CrashSound;
 
     private Rigidbody _playerRb;
     private Animator _playerAnim;
+    private AudioSource _playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
         _playerAnim = GetComponent<Animator>();
+        _playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= GravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             isOnGround = false;
             _playerAnim.SetTrigger("Jump_trig");
+            _playerAudio.PlayOneShot(JumpSound, 1.0f);
+            DirtParticle.Stop();
         }
     }
 
@@ -36,10 +44,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            DirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int", 1);
+            _playerAudio.PlayOneShot(CrashSound, 1.0f);
+            ExplosionParticle.Play();
+            DirtParticle.Stop();
         }
 
     }
